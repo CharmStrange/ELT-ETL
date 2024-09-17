b44469e9d2ec916fd3617e1b43e791a3d9a6d12e
LKH <105769152+CharmStrange@users.noreply.github.com>
2024-09-17 11:50:59 +0900

    Add files via upload

--- Process1/Database1/소스.cpp
+++ Process1/Database1/소스.cpp
@@ -4,24 +4,37 @@
 using namespace std;
 
 sqlite3* f() {
-	sqlite3* db;
-	sqlite3_open("sqlite.db", &db);
-	return db;
+    sqlite3* db;
+    int rc = sqlite3_open("sqlite.db", &db);
+    if (rc) {
+        cout << "Can't open database: " << sqlite3_errmsg(db) << endl;
+        return nullptr;
+    }
+    else {
+        cout << "Opened database successfully" << endl;
+    }
+    return db;
 }
 
 void executer(sqlite3* db, const char* query) {
-	char* E;
-	sqlite3_exec(db, query, NULL, NULL, &E);
+    char* errMsg = 0;
+    int rc = sqlite3_exec(db, query, NULL, NULL, &errMsg);
+    if (rc != SQLITE_OK) {
+        cout << errMsg << endl;
+        sqlite3_free(errMsg);
+    }
 }
 
 int main() {
-	sqlite3* db = f();
+    sqlite3* db = f();
+    if (db == nullptr) return 1; 
 
-	executer(db, "CREATE TABLE TEST \
-				T1 TEXT NOT NULL \
-				T2 TEXT NOT NULL \
-	");
-		
-	executer(db, " INSERT INTO TEST VALUES('AT1', 'AT2')");
-	sqlite3_close(db);
+    executer(db, "CREATE TABLE IF NOT EXISTS TEST ("
+        "T1 TEXT NOT NULL, "
+        "T2 TEXT NOT NULL);");
+
+    executer(db, "INSERT INTO TEST VALUES('AT1', 'AT2');");
+
+    sqlite3_close(db);
+    return 0;
 }
